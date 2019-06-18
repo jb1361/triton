85887a9ee5fa0ae8281c1c81428f16a847968a4d
jb1361 <justinbutler4@hotmail.com>
2019-06-18 18:39:04 -0400

    Create project

---
+++ triton/triton.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		triton.ino
+ Created:	6/18/2019 6:38:32 PM
+ Author:	Justin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
