9bb1ee98d73f6011e16904c2d41384ae57e17aeb
jb1361 <justinbutler4@hotmail.com>
2019-06-18 20:28:11 -0400

    Recreate aurduino project and add code to get telemetry

--- triton/triton.ino
+++ triton/triton.ino
@@ -4,12 +4,41 @@
  Author:	Justin
 */
 
-// the setup function runs once when you press reset or power the board
-void setup() {
 
+#include "TinyGPS++.h"
+#include "SoftwareSerial.h"
+
+
+SoftwareSerial serial_connection(10, 11);
+TinyGPSPlus gps;
+void setup() {
+	Serial.begin(9600);
+	serial_connection.begin(9600);
+	Serial.println("GPS Start");
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
-  
+	while (serial_connection.available())
+	{
+		gps.encode(serial_connection.read());
+	}
+	if (gps.location.isUpdated())
+	{
+		Serial.println("Satellite Count:");
+		Serial.println(gps.satellites.value());
+		Serial.println("Latitude:");
+		Serial.println(gps.location.lat(), 6);
+		Serial.println("Longitude:");
+		Serial.println(gps.location.lng(), 6);
+		Serial.println("Speed MPH:");
+		Serial.println(gps.speed.mph());
+		Serial.println("Altitude Feet:");
+		Serial.println(gps.altitude.feet());
+		Serial.println("");
+	}
+	else {
+		Serial.print("Satellite Count: ");
+		Serial.println(gps.satellites.value());		
+	}
+	delay(1);
 }
