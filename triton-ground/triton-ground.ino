8ef64c90dc28cc91aec9ce3b739d41a10dd2f6b7
jb1361 <justinbutler4@hotmail.com>
2019-06-21 00:17:44 -0400

    Add reciever code and update transmitter

--- triton-ground/triton-ground.ino
+++ triton-ground/triton-ground.ino
@@ -4,12 +4,33 @@
  Author:	Justin
 */
 
-// the setup function runs once when you press reset or power the board
+
+#include <SPI.h>
+#include <RH_ASK.h>
+
+RH_ASK driver(2000, 8, 9, 10);
+int i = 0;
+
 void setup() {
+	Serial.begin(9600);
+	Serial.println("Starting Reciever");
+	if (!driver.init()) {
+		Serial.println("Radio Head driver failed");
+	}
 
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+	uint8_t buf[24];
+	uint8_t buflen = sizeof(buf);
+	//Serial.println("Waiting for data");
+	//Serial.println(driver.rxGood());
+	if(driver.recv(buf, &buflen)) {
+		Serial.print((char*)buf);
+		Serial.print(" : " + i);
+		Serial.println();
+		i++;
+	}
+	delay(1);
 }
--- triton-orbit/triton-orbit.ino
+++ triton-orbit/triton-orbit.ino
@@ -5,17 +5,24 @@
 */
 
 
+#include <SPI.h>
 #include "TinyGPS++.h"
 #include "SoftwareSerial.h"
-
+#include <RH_ASK.h>
 
 SoftwareSerial serial_connection(10, 11);
 TinyGPSPlus gps;
+
+RH_ASK driver(2000, NULL, 12, NULL);
 
 void setup() {
 	Serial.begin(9600);
 	serial_connection.begin(9600);
 	Serial.println("GPS Start");
+	Serial.println("Starting Transmitter");
+	if (!driver.init()) {
+		Serial.println("Radio Head driver failed");
+	}
 }
 
 void loop() {
@@ -25,21 +32,27 @@ void loop() {
 	}
 	if (gps.location.isUpdated())
 	{
-		Serial.println("Satellite Count:");
-		Serial.println(gps.satellites.value());
-		Serial.println("Latitude:");
-		Serial.println(gps.location.lat(), 6);
-		Serial.println("Longitude:");
-		Serial.println(gps.location.lng(), 6);
-		Serial.println("Speed MPH:");
-		Serial.println(gps.speed.mph());
-		Serial.println("Altitude Feet:");
-		Serial.println(gps.altitude.feet());
-		Serial.println("");
+		String data = "Satellite Count: " + gps.satellites.value();
+		data += ", Latitude: " + String(gps.location.lat(), floor(sizeof(gps.location.lat())));
+		data += ", Longitude: " + String(gps.location.lng(), floor(sizeof(gps.location.lng())));
+		data += ", Speed MPH: " + String(gps.speed.mph(),floor(sizeof(gps.speed.mph())));
+		data += ", Altitude Feet : " + (String)gps.altitude.meters();
+		data += ", Timestamp : " + gps.time.value();
+		const char* msg = data.c_str();
+		driver.send((uint8_t*)msg, strlen(msg));
+		driver.waitPacketSent();
 	}
 	else {
-		Serial.print("Satellite Count: ");
-		Serial.println(gps.satellites.value());
+		//Serial.println("Satellite Count: " + gps.satellites.value());
+		/*String data = "Satellite Count: " + gps.satellites.value();
+		Serial.println(data);
+		const char* msg = data.c_str();
+		driver.send((uint8_t*)msg, strlen(msg));
+		driver.waitPacketSent();*/
+		const char* msg = "Finding Sattallites...";
+		driver.send((uint8_t*)msg, strlen(msg));
+		driver.waitPacketSent();
 	}
-	delay(1);
+	
+	delay(500);
 }
